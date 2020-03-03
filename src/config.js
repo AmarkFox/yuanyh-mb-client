bdb8554665bb18eafcc5de206a076da98947c4e6
EC2 Default User <ec2-user@ip-172-31-22-172.ap-southeast-1.compute.internal>
2020-03-03 14:31:07 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+    export default {
+        s3: {
+            REGION: "ap-southeast-1",
+            BUCKET: "yuanyh-mb"
+        },
+        apiGateway: {
+            REGION: "ap-southeast-1",
+            URL: "https://79kijk6u9h.execute-api.ap-southeast-1.amazonaws.com/prod"
+        },
+        cognito: {
+            REGION: "ap-southeast-1",
+            USER_POOL_ID: "ap-southeast-1_BSY4JIsiL",
+            APP_CLIENT_ID: "75nctd9nn1t7u3c5kojn58m1hj",
+            IDENTITY_POOL_ID: "ap-southeast-1:a94fda66-8bf9-47a0-9720-abf8918a8436"
+        }
+    };
